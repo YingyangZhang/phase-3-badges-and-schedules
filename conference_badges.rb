41e7cbd42d2f3a50db39cdd47a9ea5185b963740
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-10-15 18:40:16 -0400

    commit tree 93d883c85551094cf2e389b6454630ecf2c18d95

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    attendees.map {|person| "Hello, my name is #{person}."}
+end
+
+def assign_rooms(attendees)
+    attendees.map.with_index(1) {|person, index| "Hello, #{person}! You'll be assigned to room #{index}!"}
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(attendees).each do |assignment|
+        puts assignment
+    end
+end
